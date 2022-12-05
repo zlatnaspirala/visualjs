e4da62b6a75496a231fa44690b64452ffb8e5e00
zlatnaspirala <zlatnaspirala@gmail.com>
2022-12-05 20:32:41 +0100

    sync npm test part

--- test-npm/res.js
+++ test-npm/res.js
@@ -1,16 +1,5 @@
 
 let tools = require('visual-js-server');
-
-let config = {
-  VERSION : "3.0.0",
-  PATH_OF_NODE_APP : "G:\\web_server\\xampp\\htdocs\\PRIVATE_SERVER\\visual-js-GIT\\build\\visualjs\\test-npm\\",
-  PATH_OF_WWW : "G:\\web_server\\xampp\\htdocs\\PRIVATE_SERVER\\visual-js-GIT\\build\\visualjs\\test-npm\\",
-  EDITOR_PORT : "1013",
-  DESTROY_SESSION_AFTER_X_mSECUNDS : 20000,
-  SELF_HOST: {
-    protocol: 'http',
-    port: 80
-  }
-};
+let config = require('./config');
 
 tools.res(config);
